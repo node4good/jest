9e08a6bfa69537c63bf10a05db544ff4efa3593a
Ishai Jaffe <ishai@empeeric.com>
2012-06-13 13:27:35 +0300

    no console.log on cache

--- cache.js
+++ cache.js
@@ -4,11 +4,9 @@ var Cache = module.exports = Class.extend({
     init:function () {
     },
     get:function (key, callback) {
-        console.log("getting from cache %s", key);
         callback(null, null)
     },
     set:function (key, value, callback) {
-        console.log("storing in cache %s", key);
         callback(null)
     }
 });
